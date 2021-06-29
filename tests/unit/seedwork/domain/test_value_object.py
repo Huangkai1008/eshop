ab76e2221c9e7aa9910a53281d2f489a31ed66f8
huangkai <h1770360848@outlook.com>
2021-06-29 14:28:11 +0800

    test(seedwork): test value objects

--- tests/unit/seedwork/domain/test_value_object.py
+++ tests/unit/seedwork/domain/test_value_object.py
@@ -1,51 +1,67 @@
-from dataclasses import FrozenInstanceError
-from typing import Type, TypeVar, Union
+from dataclasses import FrozenInstanceError, dataclass
+from typing import Optional, TypeVar, Union
 
 import pytest
 
-from tests.unit.seedwork.domain.conftest import ValueObjectA, ValueObjectB
+from eshop.seedwork.domain.value_object import ValueObject
+
+
+@dataclass(frozen=True)
+class ValueObjectA(ValueObject):
+    field: int
+
+
+@dataclass(frozen=True)
+class ValueObjectB(ValueObject):
+    field: int
+
 
 VO = TypeVar('VO', bound=Union[ValueObjectA, ValueObjectB])
 
 
 class TestValueObject:
-    def test_value_objects_are_equal(self) -> None:
-        vo1 = ValueObjectA(1)
-        vo2 = ValueObjectA(1)
-
+    @pytest.mark.parametrize(
+        'vo1,vo2',
+        [
+            pytest.param(
+                None,
+                None,
+                id='both null',
+            ),
+            pytest.param(
+                ValueObjectA(1),
+                ValueObjectA(1),
+                id='equal members',
+            ),
+        ],
+    )
+    def test_value_objects_are_equal(
+        self, vo1: Optional[VO], vo2: Optional[VO]
+    ) -> None:
         equality: bool = vo1 == vo2
 
         assert equality is True
 
     @pytest.mark.parametrize(
-        'type1,value1,type2,value2',
+        'vo1,vo2',
         [
             pytest.param(
-                ValueObjectA,
-                1,
-                ValueObjectA,
-                2,
-                id='different_values',
+                ValueObjectA(1),
+                ValueObjectA(2),
+                id='different member values',
             ),
             pytest.param(
-                ValueObjectA,
-                1,
-                ValueObjectB,
-                1,
-                id='different_types',
+                ValueObjectA(1),
+                ValueObjectB(1),
+                id='different types',
             ),
         ],
     )
     def test_value_objects_are_not_equal(
         self,
-        value1: int,
-        value2: int,
-        type1: Type[VO],
-        type2: Type[VO],
+        vo1: Optional[VO],
+        vo2: Optional[VO],
     ) -> None:
-        vo1 = type1(value1)
-        vo2 = type2(value2)
-
         equality: bool = vo1 == vo2
 
         assert equality is False
