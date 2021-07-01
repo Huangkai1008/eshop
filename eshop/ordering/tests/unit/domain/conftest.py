c69c40e575b2892be925e556c70cc97817ed1c01
huangkai <h1770360848@outlook.com>
2021-07-01 16:57:17 +0800

    test(ordering): order add line

---
+++ eshop/ordering/tests/unit/domain/conftest.py
@@ -0,0 +1,25 @@
+from datetime import datetime, timedelta
+
+import pytest
+
+from eshop.ordering.domain.model.order.address import Address
+from eshop.ordering.domain.model.order.order import Order
+
+
+@pytest.fixture
+def address() -> Address:
+    return Address('street', 'city', 'state', 'country', 'zipcode')
+
+
+@pytest.fixture
+def order() -> Order:
+    return Order(
+        'user_id',
+        'username',
+        1,
+        '900x900',
+        'VISA',
+        'ms',
+        datetime.now() + timedelta(days=365),
+        Address('street', 'city', 'state', 'country', 'zipcode'),
+    )
---
+++ eshop/ordering/tests/unit/domain/test_order.py
@@ -0,0 +1,107 @@
+from contextlib import nullcontext as does_not_raise
+from decimal import Decimal
+from typing import Any
+
+import pytest
+
+from eshop.ordering.domain.exception import OrderingDomainException
+from eshop.ordering.domain.model.order.order import Order
+
+
+class TestOrder:
+    def test_add_order_line_succeeds(self, order: Order) -> None:
+        with does_not_raise():
+            order.add_order_line(
+                1,
+                'phone',
+                Decimal('1.0'),
+                Decimal('1'),
+                'picture',
+                1,
+            )
+
+    @pytest.mark.parametrize(
+        'product_id,product_name,unit_price,discount,picture_url,units,expectation',
+        [
+            pytest.param(
+                1,
+                'phone',
+                Decimal('1.0'),
+                Decimal('1.0'),
+                'picture',
+                -1,
+                pytest.raises(OrderingDomainException),
+                id='invalid units - negative',
+            ),
+            pytest.param(
+                1,
+                'phone',
+                Decimal('1.0'),
+                Decimal('1.0'),
+                'picture',
+                0,
+                pytest.raises(OrderingDomainException),
+                id='invalid units - zero',
+            ),
+            pytest.param(
+                1,
+                'phone',
+                Decimal('-1.0'),
+                Decimal('1.0'),
+                'picture',
+                1,
+                pytest.raises(OrderingDomainException),
+                id='invalid unit price - negative',
+            ),
+            pytest.param(
+                1,
+                'phone',
+                Decimal('0.0'),
+                Decimal('1.0'),
+                'picture',
+                1,
+                pytest.raises(OrderingDomainException),
+                id='invalid unit price - zero',
+            ),
+            pytest.param(
+                1,
+                'phone',
+                Decimal('1.0'),
+                Decimal('-1.0'),
+                'picture',
+                1,
+                pytest.raises(OrderingDomainException),
+                id='invalid discount - negative',
+            ),
+            pytest.param(
+                1,
+                'phone',
+                Decimal('1.0'),
+                Decimal('2.0'),
+                'picture',
+                1,
+                pytest.raises(OrderingDomainException),
+                id='invalid discount - greater than total',
+            ),
+        ],
+    )
+    def test_add_order_line_fails(
+        self,
+        order: Order,
+        product_id: int,
+        product_name: str,
+        unit_price: Decimal,
+        discount: Decimal,
+        picture_url: str,
+        units: int,
+        expectation: Any,
+    ) -> None:
+        with expectation:
+            order.add_order_line(
+                product_id,
+                product_name,
+                unit_price,
+                discount,
+                picture_url,
+                units,
+            )
