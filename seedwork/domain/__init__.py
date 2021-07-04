6c31728159b0ad55f646376108177f4a3340ef01
huangkai <h1770360848@outlook.com>
2021-07-04 08:25:47 +0800

    style: import sorts

--- seedwork/domain/__init__.py
+++ seedwork/domain/__init__.py
@@ -1,7 +1,6 @@
-from .entity import Entity
-from .value_object import ValueObject
-from .event import Event
 from .aggregate_root import AggregateRoot
-
+from .entity import Entity
+from .event import Event
+from .value_object import ValueObject
 
 __all__ = ['Entity', 'ValueObject', 'Event', 'AggregateRoot']
