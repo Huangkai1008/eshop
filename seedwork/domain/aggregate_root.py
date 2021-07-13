34034a8ab9afe73f459743e514da544cb76036eb
huangkai <h1770360848@outlook.com>
2021-07-13 20:42:51 +0800

    docs(seedwork): aggregate comments

--- seedwork/domain/aggregate_root.py
+++ seedwork/domain/aggregate_root.py
@@ -5,4 +5,9 @@ from seedwork.domain.entity import Entity
 
 @dataclass
 class AggregateRoot(Entity):
-    ...
+    """Aggregate root base class.
+
+    See Also:
+        - https://martinfowler.com/bliki/DDD_Aggregate.html
+
+    """
