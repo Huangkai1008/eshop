2324a22cb42fb3205359fea8f34b7d4ad5a72dd6
huangkai <h1770360848@outlook.com>
2021-09-29 23:42:22 +0800

    test(seedwork): base query model tests

--- tests/unit/application/test_model.py
+++ tests/unit/application/test_model.py
@@ -1,25 +1,76 @@
 from dataclasses import dataclass
 
-from seedwork.application import BaseModel
+import pytest
+
+from seedwork.application import BaseModel, QueryModel
 from seedwork.domain import Entity
 
 
 @dataclass
-class Account(Entity):
+class Sample(Entity):
     username: str
     password: str
 
 
-class AccountReadModel(BaseModel):
+class SampleModel(BaseModel):
     id: str
     username: str
 
 
-class TestModel:
-    def test_entity_to_model(self) -> None:
-        account = Account(username='user', password='password')
+class SampleQueryModel(QueryModel):
+    ...
 
-        model = AccountReadModel.from_entity(account)
+
+class TestModel:
+    def test_create_model_from_entity(self) -> None:
+        sample = Sample(username='user', password='password')
+
+        model = SampleModel.from_entity(sample)
 
         assert model.username == 'user'
         assert not hasattr(model, 'password')
+
+    @pytest.mark.parametrize(
+        'data,expected',
+        [
+            pytest.param(
+                dict(),
+                dict(page=1, size=10),
+                id='empty values',
+            ),
+            pytest.param(
+                dict(page=2, size=20),
+                dict(page=2, size=20),
+                id='custom values',
+            ),
+        ],
+    )
+    def test_create_query_model_succeed(self, data: dict, expected: dict) -> None:
+        query = SampleQueryModel(**data)
+
+        assert query.model_dump() == expected
+
+    @pytest.mark.parametrize(
+        'data',
+        [
+            pytest.param(
+                dict(page=-1, size=10),
+                id='invalid page - negative',
+            ),
+            pytest.param(
+                dict(page=0, size=10),
+                id='invalid page - zero',
+            ),
+            pytest.param(
+                dict(page=1, size=-10),
+                id='invalid size - negative',
+            ),
+            pytest.param(
+                dict(page=1, size=0),
+                id='invalid size - zero',
+            ),
+        ],
+    )
+    def test_create_query_model_fails(self, data: dict) -> None:
+        with pytest.raises(ValueError):
+            SampleQueryModel(**data)
