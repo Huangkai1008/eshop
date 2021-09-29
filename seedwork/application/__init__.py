4caba1b632d9881fc75ca6483f2a3741bb81f913
huangkai <h1770360848@outlook.com>
2021-09-29 23:41:58 +0800

    feat(seedwork): base query model

--- seedwork/application/__init__.py
+++ seedwork/application/__init__.py
@@ -1,4 +1,4 @@
 from .command import Command
-from .model import BaseModel
+from .model import BaseModel, PaginatedItemsViewModel, QueryModel
 
-__all__ = ['Command', 'BaseModel']
+__all__ = ['Command', 'BaseModel', 'QueryModel', 'PaginatedItemsViewModel']
--- seedwork/application/model.py
+++ seedwork/application/model.py
@@ -1,12 +1,26 @@
 from dataclasses import asdict
+from typing import Annotated, Generic, TypeVar, Union
 
 from pydantic import BaseModel as _BaseModel
+from pydantic import PositiveInt
 from typing_extensions import Self
 
 from seedwork.domain import Entity
+
+ItemT = TypeVar('ItemT')
 
 
 class BaseModel(_BaseModel):
     @classmethod
     def from_entity(cls, entity: Entity) -> Self:
         return cls(**asdict(entity))
+
+
+class QueryModel(BaseModel):
+    page: Annotated[Union[PositiveInt, None], 'page number'] = 1
+    size: Annotated[Union[PositiveInt, None], 'page size'] = 10
+
+
+class PaginatedItemsViewModel(BaseModel, Generic[ItemT]):
+    total: int
+    items: list[ItemT]
