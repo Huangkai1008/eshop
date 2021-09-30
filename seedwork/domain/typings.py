fde81aa69c4d285feab072ada608f777078ef5ad
huangkai <h1770360848@outlook.com>
2021-09-30 22:13:53 +0800

    feat(seedwork): covariant generic type

--- seedwork/domain/typings.py
+++ seedwork/domain/typings.py
@@ -4,3 +4,5 @@ from .entity import Entity, EntityId
 
 T = TypeVar('T', bound=Entity)
 ID = TypeVar('ID', bound=EntityId)
+T_co = TypeVar('T_co', bound=Entity, covariant=True)
+ID_co = TypeVar('ID_co', bound=EntityId, covariant=True)
